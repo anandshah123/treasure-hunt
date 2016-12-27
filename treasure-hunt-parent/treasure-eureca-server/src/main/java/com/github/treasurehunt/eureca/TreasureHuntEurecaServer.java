feded62635d3bd3e9117b769e30db263d0dd4b8d
Anand Shah <anand.shah110@gmail.com>
2016-12-27 20:27:09 +0530

    Project initialization

---
+++ treasure-hunt-parent/treasure-eureca-server/src/main/java/com/github/treasurehunt/eureca/TreasureHuntEurecaServer.java
@@ -0,0 +1,14 @@
+package com.github.treasurehunt.eureca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class TreasureHuntEurecaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(TreasureHuntEurecaServer.class, args);
+    }
+
+}
