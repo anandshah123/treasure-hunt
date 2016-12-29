a125c614952f0906600a800ba1e980e64f830040
Anand Shah <anand.shah110@gmail.com>
2016-12-29 15:54:03 +0530

    Reward distribution as per config

--- treasure-hunt-parent/treasure-app/src/main/java/com/github/treasurehunt/controller/TreasureController.java
+++ treasure-hunt-parent/treasure-app/src/main/java/com/github/treasurehunt/controller/TreasureController.java
@@ -1,23 +1,41 @@
 package com.github.treasurehunt.controller;
 
 import com.github.treasurehunt.dto.ConfigDTO;
+import com.github.treasurehunt.dto.Reward;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 @RequestMapping("/treasure")
 public class TreasureController {
 
+    private static ConfigDTO configDTO = new ConfigDTO();
+    private static final AtomicInteger totalHits = new AtomicInteger();
+    private static final Random random = new Random();
+
     @Secured("ROLE_ADMIN")
     @PostMapping("/config")
     public ConfigDTO setParams(@RequestBody ConfigDTO configDTO) {
+        TreasureController.configDTO = configDTO;
         return configDTO;
     }
 
     @Secured("ROLE_USER")
     @GetMapping("/earn")
-    public @ResponseBody String earnMoney(Authentication authentication) {
-        return "";
+    public Reward earnMoney(Authentication authentication) throws InterruptedException {
+        Reward reward;
+        if (totalHits.incrementAndGet() < configDTO.getFirstNLucky()) {
+            reward = new Reward(configDTO.getFirstNLuckyPoints());
+        } else {
+            reward = new Reward(configDTO.getMinPoints() + random.nextInt(configDTO.getMaxPoints() - configDTO.getMinPoints()));
+        }
+        if (configDTO.getResponseDelay() > 0) {
+            Thread.sleep(configDTO.getResponseDelay());
+        }
+        return reward;
     }
 }
