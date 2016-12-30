1a13a9f5a4e88016fd0af77af61b45c6b8ff26f2
Balkrushn Viroja <vbalkrushn@yahoo.com>
2016-12-30 07:20:34 +0000

    Application properties changes

--- treasure-hunt-parent/treasure-app/src/main/java/com/github/treasurehunt/controller/TreasureController.java
+++ treasure-hunt-parent/treasure-app/src/main/java/com/github/treasurehunt/controller/TreasureController.java
@@ -64,19 +64,4 @@ public class TreasureController {
         }
         return reward;
     }
-
-    @PostMapping("/register")
-    public @ResponseBody String register(UserInfo userInfo) {
-        try {
-            if(!userDao.exists(userInfo.getUsername())){
-                userDao.save(userInfo);
-                return "Registered Successfully...!!!";
-            }else{
-                return "Already Registered. Please contact admin to update details...!!!";
-            }
-        }catch (Exception e){
-            log.error(e.getMessage(), e);
-            throw new  RuntimeException("Can't Register. Internal server error");
-        }
-    }
 }
---
+++ treasure-hunt-parent/treasure-hunt-team-registration/src/main/java/com/github/treasurehunt/controller/TreasureController.java
@@ -0,0 +1,34 @@
+package com.github.treasurehunt.controller;
+
+import com.github.treasurehunt.dao.UserDao;
+import com.github.treasurehunt.entity.UserInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/treasure")
+@Slf4j
+public class TreasureController {
+
+    @Autowired
+    private UserDao userDao;
+
+    @PostMapping("/register")
+    public @ResponseBody String register(UserInfo userInfo) {
+        try {
+            if(!userDao.exists(userInfo.getUsername())){
+                userDao.save(userInfo);
+                return "Registered Successfully...!!!";
+            }else{
+                return "Already Registered. Please contact admin to update details...!!!";
+            }
+        }catch (Exception e){
+            log.error(e.getMessage(), e);
+            throw new  RuntimeException("Can't Register. Internal server error");
+        }
+    }
+}
